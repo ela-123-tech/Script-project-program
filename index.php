46e041e0d51763368f5d8e12c8edb65187f7fc84
SOLAI ELAVARASAN <23spcctelavarasang@gmail.com>
2025-04-07 16:10:49 +0530

    admin

--- index.php
+++ index.php
@@ -1,167 +1,262 @@
+
 <?php 
-include 'Includes/dbcon.php';
-session_start();
+include '../Includes/dbcon.php';
+include '../Includes/session.php';
+
+
+    $query = "SELECT tblclass.className,tblclassarms.classArmName 
+    FROM tblclassteacher
+    INNER JOIN tblclass ON tblclass.Id = tblclassteacher.classId
+    INNER JOIN tblclassarms ON tblclassarms.Id = tblclassteacher.classArmId
+    Where tblclassteacher.Id = '$_SESSION[userId]'";
+
+    $rs = $conn->query($query);
+    $num = $rs->num_rows;
+    $rrw = $rs->fetch_assoc();
+
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="img/logo/free1.png" rel="icon">
-    <title>SPC COMP - Login</title>
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-    <link href="css/ruang-admin.min.css" rel="stylesheet">
-
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/free1.png" rel="icon">
+  <title>Dashboard</title>
+  <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="css/ruang-admin.min.css" rel="stylesheet">
 </head>
 
-<body class="bg-gradient-login" style="background-image: url('img/logo/loral1.jpe00g');">
-    <!-- Login Content -->
-    <div class="container-login">
-        <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
-                <div class="card shadow-sm my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="login-form">
-                                    <h5 align="center">STUDENT ATTENDANCE SYSTEM</h5>
-                                    <div class="text-center">
-                                        <img src="img/logo/free1.png" style="width:100px;height:100px">
-                                        <br><br>
-                                        <h1 class="h4 text-gray-900 mb-4">Login Panel</h1>
-                                    </div>
-                                    <form class="user" method="Post" action="">
-                                        <div class="form-group">
-                                            <select required name="userType" class="form-control mb-3">
-                                                <option value="">--Select User Roles--</option>
-                                                <option value="Administrator">Administrator</option>
-                                                <option value="ClassTeacher">ClassTeacher</option>
-                                            </select>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="text" class="form-control" required name="username" id="exampleInputEmail" placeholder="Enter Email Address">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" name="password" required class="form-control" id="exampleInputPassword" placeholder="Enter Password">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <!-- <label class="custom-control-label" for="customCheck">Remember
-                          Me</label> -->
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="submit" class="btn btn-success btn-block" value="Login" name="login" />
-                                        </div>
-                                    </form>
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+   <?php include "Includes/sidebar.php";?>
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+           <?php include "Includes/topbar.php";?>
+        <!-- Topbar -->
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Administrator Dashboard</h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="./">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+            </ol>
+          </div>
 
-                                    <?php
-
-  if(isset($_POST['login'])){
-
-    $userType = $_POST['userType'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $password = md5($password);
-
-    if($userType == "Administrator"){
-
-      $query = "SELECT * FROM tbladmin WHERE emailAddress = '$username' AND password = '$password'";
-      $rs = $conn->query($query);
-      $num = $rs->num_rows;
-      $rows = $rs->fetch_assoc();
-
-      if($num > 0){
-
-        $_SESSION['userId'] = $rows['Id'];
-        $_SESSION['firstName'] = $rows['firstName'];
-        $_SESSION['lastName'] = $rows['lastName'];
-        $_SESSION['emailAddress'] = $rows['emailAddress'];
-
-        echo "<script type = \"text/javascript\">
-        window.location = (\"Admin/index.php\")
-        </script>";
-      }
-
-      else{
-
-        echo "<div class='alert alert-danger' role='alert'>
-        Invalid Username/Password!
-        </div>";
-
-      }
-    }
-    else if($userType == "ClassTeacher"){
-
-      $query = "SELECT * FROM tblclassteacher WHERE emailAddress = '$username' AND password = '$password'";
-      $rs = $conn->query($query);
-      $num = $rs->num_rows;
-      $rows = $rs->fetch_assoc();
-
-      if($num > 0){
-
-        $_SESSION['userId'] = $rows['Id'];
-        $_SESSION['firstName'] = $rows['firstName'];
-        $_SESSION['lastName'] = $rows['lastName'];
-        $_SESSION['emailAddress'] = $rows['emailAddress'];
-        $_SESSION['classId'] = $rows['classId'];
-        $_SESSION['classArmId'] = $rows['classArmId'];
-
-        echo "<script type = \"text/javascript\">
-        window.location = (\"ClassTeacher/index.php\")
-        </script>";
-      }
-
-      else{
-
-        echo "<div class='alert alert-danger' role='alert'>
-        Invalid Username/Password!
-        </div>";
-
-      }
-    }
-    else{
-
-        echo "<div class='alert alert-danger' role='alert'>
-        Invalid Username/Password!
-        </div>";
-
-    }
-}
+          <div class="row mb-3">
+          <!-- Students Card -->
+          <?php 
+$query1=mysqli_query($conn,"SELECT * from tblstudents");                       
+$students = mysqli_num_rows($query1);
 ?>
-
-                                    <!-- <hr>
-                    <a href="index.html" class="btn btn-google btn-block">
-                      <i class="fab fa-google fa-fw"></i> Login with Google
-                    </a>
-                    <a href="index.html" class="btn btn-facebook btn-block">
-                      <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                    </a> -->
-
-
-                                    <div class="text-center">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Students</div>
+                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $students;?></div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <!-- <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
+                        <span>Since last month</span> -->
+                      </div>
                     </div>
+                    <div class="col-auto">
+                      <i class="fas fa-users fa-2x text-info"></i>
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
+            <!-- Class Card -->
+             <?php 
+$query1=mysqli_query($conn,"SELECT * from tblclass");                       
+$class = mysqli_num_rows($query1);
+?>
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Classes</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $class;?></div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <!-- <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                        <span>Since last month</span> -->
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-chalkboard fa-2x text-primary"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!-- Class Arm Card -->
+             <?php 
+$query1=mysqli_query($conn,"SELECT * from tblclassarms");                       
+$classArms = mysqli_num_rows($query1);
+?>
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Class Arms</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $classArms;?></div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <!-- <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
+                        <span>Since last years</span> -->
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-code-branch fa-2x text-success"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+            <!-- Std Att Card  -->
+            <?php 
+$query1=mysqli_query($conn,"SELECT * from tblattendance");                       
+$totAttendance = mysqli_num_rows($query1);
+?>
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card h-100">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Total Student Attendance</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $totAttendance;?></div>
+                      <div class="mt-2 mb-0 text-muted text-xs">
+                        <!-- <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 1.10%</span>
+                        <span>Since yesterday</span> -->
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-secondary"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Teachers Card  -->
+            <?php 
+            $query1=mysqli_query($conn,"SELECT * from tblclassteacher");                       
+            $classTeacher = mysqli_num_rows($query1);
+            ?>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                          <div class="card h-100">
+                            <div class="card-body">
+                              <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                  <div class="text-xs font-weight-bold text-uppercase mb-1">Class Teachers</div>
+                                  <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $classTeacher;?></div>
+                                  <div class="mt-2 mb-0 text-muted text-xs">
+                                    <!-- <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
+                                    <span>Since last years</span> -->
+                                  </div>
+                                </div>
+                                <div class="col-auto">
+                                  <i class="fas fa-chalkboard-teacher fa-2x text-danger"></i>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+          
+
+                         <!-- Session and Terms Card  -->
+            <?php 
+            $query1=mysqli_query($conn,"SELECT * from tblsessionterm");                       
+            $sessTerm = mysqli_num_rows($query1);
+            ?>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                          <div class="card h-100">
+                            <div class="card-body">
+                              <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                  <div class="text-xs font-weight-bold text-uppercase mb-1">Session & Terms</div>
+                                  <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $sessTerm;?></div>
+                                  <div class="mt-2 mb-0 text-muted text-xs">
+                                    <!-- <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
+                                    <span>Since last years</span> -->
+                                  </div>
+                                </div>
+                                <div class="col-auto">
+                                  <i class="fas fa-calendar-alt fa-2x text-warning"></i>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+
+
+                        <!-- Terms Card  -->
+            <?php 
+            $query1=mysqli_query($conn,"SELECT * from tblterm");                       
+            $termonly = mysqli_num_rows($query1);
+            ?>
+                        <div class="col-xl-3 col-md-6 mb-4">
+                          <div class="card h-100">
+                            <div class="card-body">
+                              <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                  <div class="text-xs font-weight-bold text-uppercase mb-1">Terms</div>
+                                  <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $termonly;?></div>
+                                  <div class="mt-2 mb-0 text-muted text-xs">
+                                    <!-- <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
+                                    <span>Since last years</span> -->
+                                  </div>
+                                </div>
+                                <div class="col-auto">
+                                  <i class="fas fa-th fa-2x text-info"></i>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+          <!--Row-->
+
+          <!-- <div class="row">
+            <div class="col-lg-12 text-center">
+              <p>Do you like this template ? you can download from <a href="https://github.com/indrijunanda/RuangAdmin"
+                  class="btn btn-primary btn-sm" target="_blank"><i class="fab fa-fw fa-github"></i>&nbsp;GitHub</a></p>
+            </div>
+          </div> -->
+
         </div>
+        <!---Container Fluid-->
+      </div>
+      <!-- Footer -->
+      <?php include 'includes/footer.php';?>
+      <!-- Footer -->
     </div>
-    <!-- Login Content -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="js/ruang-admin.min.js"></script>
+  </div>
+
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="js/ruang-admin.min.js"></script>
+  <script src="../vendor/chart.js/Chart.min.js"></script>
+  <script src="js/demo/chart-area-demo.js"></script>  
 </body>
 
 </html>
